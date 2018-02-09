6a988413ed6e17d1557a8abbb61c27cc53e20bd4
Clearfix <nagana12@gmail.com>
2018-02-09 02:07:55 +0200

    first commit

---
+++ gulp/path/libs.js
@@ -0,0 +1,9 @@
+module.exports = {
+  'jsLibs': [
+    './node_modules/jquery/dist/jquery.min.js'
+  ],
+  'cssLibs': [
+    './node_modules/normalize.css/normalize.css',
+    './node_modules/animate.css/animate.min.css'
+  ]
+};
